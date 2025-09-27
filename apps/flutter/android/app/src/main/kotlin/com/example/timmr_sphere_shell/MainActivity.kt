b5a704a0100182767ba5c4c8692b7e48165e0c73
eliezer.amaya <eliezer.amaya@outlook.com>
2025-09-26 21:17:52 -0600

    feat(app): connect Flutter shell to web URL

---
+++ apps/flutter/android/app/src/main/kotlin/com/example/timmr_sphere_shell/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.timmr_sphere_shell
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- apps/flutter/lib/presentation/widgets/sphere_view_iframe.dart
+++ apps/flutter/lib/presentation/widgets/sphere_view_iframe.dart
@@ -1,8 +1,10 @@
 // Web: IFrame que apunta a la app React publicada
 // ignore: avoid_web_libraries_in_flutter
 import 'dart:html' as html;
-import 'dart:ui' as ui;
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
+// ignore: uri_does_not_exist
+import 'dart:ui_web' as ui_web; // Solo existe en compilación Web
 
 const String kWebAppUrl = 'https://eliezeramaya.github.io/vault/';
 
@@ -11,16 +13,19 @@ class SphereViewPlatform extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // registra un viewType único
-    // ignore: undefined_prefixed_name
-    ui.platformViewRegistry.registerViewFactory('sphere-iframe', (int viewId) {
+    // Registra un viewType único en Web
+    if (kIsWeb) {
+      // ignore: undefined_prefixed_name
+      ui_web.platformViewRegistry
+          .registerViewFactory('sphere-iframe', (int viewId) {
       final iframe = html.IFrameElement()
         ..src = kWebAppUrl
         ..style.border = '0'
         ..style.width = '100%'
         ..style.height = '100%';
       return iframe;
-    });
+      });
+    }
 
     return const HtmlElementView(viewType: 'sphere-iframe');
   }
